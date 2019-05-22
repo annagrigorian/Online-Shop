4571cd185a3e6abfe6e2658d53e11151bb71f1ff
Anna Grigorian <anna.grigorian11@gmail.com>
2019-05-22 05:27:14 +0400

    Add register fields

---
+++ OnlineShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineShop.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().Property(p => p.FirstName).HasMaxLength(15);
+            builder.Entity<ApplicationUser>().Property(p => p.LastName).HasMaxLength(20);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ OnlineShop/Data/Migrations/20190522003403_Do.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineShop.Data.Migrations
+{
+    public partial class Do : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Addresss",
+                table: "AspNetUsers",
+                newName: "Address");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                maxLength: 15,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserType",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserType",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "AspNetUsers",
+                newName: "Addresss");
+        }
+    }
+}
