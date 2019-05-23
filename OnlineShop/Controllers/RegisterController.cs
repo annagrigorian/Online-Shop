7a00481efd50463d5e3e245951706c420a2bf2a3
Anna Grigorian <anna.grigorian11@gmail.com>
2019-05-23 22:26:58 +0400

    Add UserCreate

--- OnlineShop/Controllers/RegisterController.cs
+++ OnlineShop/Controllers/RegisterController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using OnlineShop.Data;
@@ -11,6 +12,13 @@ namespace OnlineShop.Controllers
 {
     public class RegisterController : Controller
     {
+        private readonly UserManager<RegisteredPerson> _userManager;
+
+        public RegisterController(UserManager<RegisteredPerson> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public SelectList SelectBloodGroup()
         {
             List<BloodGroup> bloodGroups = new List<BloodGroup>();
@@ -44,16 +52,34 @@ namespace OnlineShop.Controllers
         }
 
         [HttpPost]
-        public ActionResult Register(RegisteredPerson model)
+        public async Task<ActionResult> Register(RegisteredPerson model)
         {
-            
+            if (ModelState.IsValid)
+            {
+                var user = new RegisteredPerson
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    BloodGroup = model.BloodGroup,
+                    UserName = model.UserName,
+                    Email = model.Email
+                };
+
+                var result = await _userManager.CreateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return  RedirectToAction("Index","Home");
+                }
+            }
+
             //stex arden karas dbi heti gorcery anes , qces db kam stugumner anes
             //hima asem inch arim 
             //nax en register viewn du grel eyr account areai mej, u aysinqn home controlleric tenc prosty vor kanches return View inqy chi gna yndex
             //dra hamar anuny poxeci dreci register, en myus registeri mejinnery qceci es meki mej, mekel miatel register action sarqeci
             //bayc es meky post atributov, isk et nshanakuma vor en formic vor submity kancchvi kga posti mej kmtni
             //stex el qo modely arden celikom karas stanas, yndex validacianer anes, inchvor mas ka vor chhaskacar?
-            return Redirect("Index");
+            return RedirectToAction("Index","Home");
         }
     }
 }
